d3c8ac6b619f659eabeb8132eb6b29dd6a1b5aac
helena-varela <helena.varelakg@gmail.com>
2025-09-05 17:13:58 -0300

    recriando página inicial da netflix

---
+++ section 01/index.js
@@ -0,0 +1,5 @@
+// basicamente essa seção foi dedicada a instalção no nodejs e preparação do ambiente
+// no meu caso fiz por meio do terminal no linux
+
+console.log('Hello World');
+console.log("Aula de JS!");
